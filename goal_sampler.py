ec9d72bf2da9a44bb7c3d16db5d9b267cd90ca47
akakzia <ahmed.akakzia@gmail.com>
2020-05-13 15:22:30 +0200

    increase n_buckets from 5 to 7 bias init only if discovered > 20 env grasp proba from 0.6 to 0.3

--- goal_sampler.py
+++ goal_sampler.py
@@ -134,7 +134,6 @@ class GoalSampler:
 
         if self.rank == 0:
             all_episode_list = []
-            ag_ids = []
             for eps in all_episodes:
                 all_episode_list += eps
             # logger.info('Len eps' + str(len(all_episode_list)))
@@ -142,7 +141,6 @@ class GoalSampler:
             # label each episode with the oracle id of the last ag (to know where to store it in buffers)
             if not self.curriculum_learning or self.automatic_buckets:
                 for e in all_episode_list:
-                    ag_ids.append(self.g_str_to_oracle_id[str(e['ag'][-1])])
                     # if we're looking for pairs
                     id_ag_0 = self.g_str_to_oracle_id[str(e['ag'][0])]
 
@@ -152,10 +150,10 @@ class GoalSampler:
                             if str(ag) not in self.valid_goals_str:
                                 stop = 1
                             # it is, update info
-                            else:
-                                self.discovered_goals.append(ag.copy())
-                                self.discovered_goals_str.append(str(ag))
-                                self.discovered_goals_oracle_id.append(self.g_str_to_oracle_id[str(ag)])
+                        else:
+                            self.discovered_goals.append(ag.copy())
+                            self.discovered_goals_str.append(str(ag))
+                            self.discovered_goals_oracle_id.append(self.g_str_to_oracle_id[str(ag)])
 
                         # update discovered pairs
                         id_ag = self.g_str_to_oracle_id[str(ag)]
@@ -163,8 +161,8 @@ class GoalSampler:
                             self.discovered_pairs_oracle_ids.append([id_ag_0, id_ag])
 
                 # update buckets
-                if self.automatic_buckets and t % 1000 == 0:
-                    self.update_buckets(ag_ids)
+                if self.automatic_buckets:
+                    self.update_buckets()
 
             if self.curriculum_learning:
                 # update list of successes and failures
@@ -188,48 +186,31 @@ class GoalSampler:
             last_ag = e['ag_binary'][-1]
             oracle_id = self.g_str_to_oracle_id[str(last_ag)]
             e['last_ag_oracle_id'] = oracle_id
+            # g = e['g'][0]
+            # oracle_id = self.g_str_to_oracle_id[str(g)]
+            # e['g_oracle_id'] = oracle_id
 
         return episodes
 
-    def update_buckets(self, ag_ids):
+    def update_buckets(self):
 
         if self.use_pairs:
             discovered = np.array(self.discovered_pairs_oracle_ids).copy()
         else:
             discovered = np.array(self.discovered_goals_oracle_id).copy()
 
-        discovered = np.delete(discovered, np.where(discovered == 0))
-        distribution = np.array([1 / len(ag_ids) for _ in range(len(discovered))])
-        for i, disc_goal_id in enumerate(discovered):
-            count = np.argwhere(ag_ids == disc_goal_id).flatten().shape[0]
-            distribution[i] = count * distribution[i]
         # Dispatch the discovered goals (or pairs) in the buckets chronologically
-        idxs = np.flip(np.argsort(distribution))
-        discovered = discovered[idxs]
+        j = 0
+        portion_length = len(discovered) // self.num_buckets
+        k = len(discovered) %  self.num_buckets
         for i in range(self.num_buckets):
-            self.buckets[i] = discovered
-        i = 0
-        self.buckets[i] = [0, discovered[0]]
-        for x in discovered[1:]:
-            if abs(x - self.buckets[i][-1]) / x <= 1:
-                self.buckets[i].append(x)
-            elif i < 4:
-                i += 1
-                self.buckets[i] = [x]
+            if k > 0:
+                l = portion_length + 1
+                k -= 1
             else:
-                self.buckets[i - 1].append(x)
-
-        # j = 0
-        # portion_length = len(discovered) // self.num_buckets
-        # k = len(discovered) %  self.num_buckets
-        # for i in range(self.num_buckets):
-        #     if k > 0:
-        #         l = portion_length + 1
-        #         k -= 1
-        #     else:
-        #         l = portion_length
-        #     self.buckets[i] = discovered[j:j + l].tolist()
-        #     j += l
+                l = portion_length
+            self.buckets[i] = discovered[j:j + l].tolist()
+            j += l
 
     def update_LP(self):
 
@@ -260,8 +241,8 @@ class GoalSampler:
             if self.LP.sum() == 0:
                 self.p = np.ones([self.num_buckets]) / self.num_buckets
             else:
-                # self.p = self.LP / self.LP.sum()
-                self.p = self.epsilon * (1 - self.C) / (1 - self.C).sum() + (1 - self.epsilon) * self.LP / self.LP.sum()
+                self.p = self.LP / self.LP.sum()
+                # self.p = self.epsilon * (1 - self.C) / (1 - self.C).sum() + (1 - self.epsilon) * self.LP / self.LP.sum()
 
             if self.p.sum() > 1:
                 self.p[np.argmax(self.p)] -= self.p.sum() - 1
