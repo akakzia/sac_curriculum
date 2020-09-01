8c813d3511d715919560f4d064bf6f30df81be7a
akakzia <ahmed.akakzia@gmail.com>
2020-09-01 16:23:29 +0200

    New FetchManipulate env with atomic parial goals

--- env/envs.py
+++ env/envs.py
@@ -1,23 +1,10 @@
-from env import fetch_manipulate_env
+from env import fetch_manipulate_env, fetch_manipulate_env_continuous, fetch_manipulate_env_atomic
 from gym import utils
 
-PREDICATE_THRESHOLD = 0.09
-
-class FetchManipulateCloseEnv(fetch_manipulate_env.FetchManipulateEnv, utils.EzPickle):
-    def __init__(self, reward_type='sparse'):
-        initial_qpos = {
-            'robot0:slide0': 0.405,
-            'robot0:slide1': 0.48,
-            'robot0:slide2': 0.0,
-            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
-            'object1:joint': [1.25, 0.53, 0.46, 1., 0., 0., 0.],
-        }
-        fetch_manipulate_env.FetchManipulateEnv.__init__(
-            self, 'fetch/stack3.xml', num_blocks=3, block_gripper=False, n_substeps=20,
-            gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
-            obj_range=0.15, target_range=0.15, predicate_threshold=PREDICATE_THRESHOLD,
-            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['close'])
-        utils.EzPickle.__init__(self)
+PREDICATE_THRESHOLD = 0.09  # The minimal threshold to consider two blocks close to each other
+PROBA_COPLANAR = 0.7  # The probability of initializing all blocks without stacks
+PROBA_STACK_TWO = 0.7  # The probability of having exactly one stack of two given that there is at least one stack
+PROBA_GRASP = 0.5  # The probability of having a block grasped at initialization
 
 class FetchManipulateEnv(fetch_manipulate_env.FetchManipulateEnv, utils.EzPickle):
     def __init__(self, reward_type='sparse'):
@@ -32,10 +19,12 @@ class FetchManipulateEnv(fetch_manipulate_env.FetchManipulateEnv, utils.EzPickle
             self, 'fetch/stack3.xml', num_blocks=3, block_gripper=False, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, predicate_threshold=PREDICATE_THRESHOLD,
-            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['close', 'above'])
+            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['close', 'above'],
+            p_coplanar=PROBA_COPLANAR, p_stack_two=PROBA_STACK_TWO, p_grasp=PROBA_GRASP,
+        )
         utils.EzPickle.__init__(self)
 
-class FetchManipulateAboveEnv(fetch_manipulate_env.FetchManipulateEnv, utils.EzPickle):
+class FetchManipulateEnvContinuous(fetch_manipulate_env_continuous.FetchManipulateEnvContinuous, utils.EzPickle):
     def __init__(self, reward_type='sparse'):
         initial_qpos = {
             'robot0:slide0': 0.405,
@@ -44,41 +33,29 @@ class FetchManipulateAboveEnv(fetch_manipulate_env.FetchManipulateEnv, utils.EzP
             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
             'object1:joint': [1.25, 0.53, 0.46, 1., 0., 0., 0.],
         }
-        fetch_manipulate_env.FetchManipulateEnv.__init__(
+        fetch_manipulate_env_continuous.FetchManipulateEnvContinuous.__init__(
+            self, 'fetch/stack3_with_targets.xml', num_blocks=3, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
+            obj_range=0.15, target_range=0.15, predicate_threshold=PREDICATE_THRESHOLD,
+            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['close', 'above'],
+            p_coplanar=PROBA_COPLANAR, p_stack_two=PROBA_STACK_TWO, p_grasp=PROBA_GRASP,
+        )
+        utils.EzPickle.__init__(self)
+
+class FetchManipulateEnvAtomic(fetch_manipulate_env_atomic.FetchManipulateEnvAtomic, utils.EzPickle):
+    def __init__(self, reward_type='sparse'):
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+            'object1:joint': [1.25, 0.53, 0.46, 1., 0., 0., 0.],
+        }
+        fetch_manipulate_env_atomic.FetchManipulateEnvAtomic.__init__(
             self, 'fetch/stack3.xml', num_blocks=3, block_gripper=False, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, predicate_threshold=PREDICATE_THRESHOLD,
-            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['above'])
-        utils.EzPickle.__init__(self)
-
-class FetchManipulateRightCloseEnv(fetch_manipulate_env.FetchManipulateEnv, utils.EzPickle):
-    def __init__(self, reward_type='sparse'):
-        initial_qpos = {
-            'robot0:slide0': 0.405,
-            'robot0:slide1': 0.48,
-            'robot0:slide2': 0.0,
-            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
-            'object1:joint': [1.25, 0.53, 0.46, 1., 0., 0., 0.],
-        }
-        fetch_manipulate_env.FetchManipulateEnv.__init__(
-            self, 'fetch/stack3.xml', num_blocks=3, block_gripper=False, n_substeps=20,
-            gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
-            obj_range=0.15, target_range=0.15, predicate_threshold=PREDICATE_THRESHOLD,
-            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['right_close'])
-        utils.EzPickle.__init__(self)
-
-class FetchManipulateZoneEnv(fetch_manipulate_env.FetchManipulateEnv, utils.EzPickle):
-    def __init__(self, reward_type='sparse'):
-        initial_qpos = {
-            'robot0:slide0': 0.405,
-            'robot0:slide1': 0.48,
-            'robot0:slide2': 0.0,
-            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
-            'object1:joint': [1.25, 0.53, 0.46, 1., 0., 0., 0.],
-        }
-        fetch_manipulate_env.FetchManipulateEnv.__init__(
-            self, 'fetch/zone_5.xml', num_blocks=5, block_gripper=False, n_substeps=20,
-            gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
-            obj_range=0.02, target_range=0.15, predicate_threshold=PREDICATE_THRESHOLD,
-            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['inside_zone'])
+            initial_qpos=initial_qpos, reward_type=reward_type, predicates=['close', 'above'],
+            p_coplanar=PROBA_COPLANAR, p_stack_two=PROBA_STACK_TWO, p_grasp=PROBA_GRASP,
+        )
         utils.EzPickle.__init__(self)
