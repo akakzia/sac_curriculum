f4448b3ff87f3de7254faa7cf7deae84dbf2ebc6
akakzia <ahmed.akakzia@gmail.com>
2020-10-27 09:34:20 +0100

    update arguments

--- arguments.py
+++ arguments.py
@@ -12,17 +12,17 @@ Here are the param for the training
 def get_args():
     parser = argparse.ArgumentParser()
     # the environment setting
-    parser.add_argument('--env-name', type=str, default='FetchManipulate3ObjectsAtomic-v0', help='the environment name')
+    parser.add_argument('--env-name', type=str, default='FetchManipulate2ObjectsAtomic-v0', help='the environment name')
     parser.add_argument('--agent', type=str, default='SAC', help='the agent name')
     parser.add_argument('--n-epochs', type=int, default=1000, help='the number of epochs to train the agent')
     parser.add_argument('--n-cycles', type=int, default=50, help='the times to collect samples per epoch')
     parser.add_argument('--n-batches', type=int, default=30, help='the times to update the network')
     parser.add_argument('--biased-init', type=bool, default=True, help='use biased environment initializations')
-    parser.add_argument('--automatic-buckets', type=bool, default=True, help='automatically generate buckets during training')
+    parser.add_argument('--automatic-buckets', type=bool, default=False, help='automatically generate buckets during training')
     parser.add_argument('--num-buckets', type=int, default=5, help='number of buckets for automatic generation')
     parser.add_argument('--symmetry-trick', type=bool, default=False, help='experimental stuff from Cédric')
     parser.add_argument('--combinations-trick', type=bool, default=True, help='test Thomas trick')
-    parser.add_argument('--start-biased-init', type=int, default=100, help='number of epochs before bias initialization')
+    parser.add_argument('--start-biased-init', type=int, default=0, help='number of epochs before bias initialization')
     parser.add_argument('--normalize_goal', type=bool, default=True, help='do evaluation at the end of the epoch w/ frequency')
 
     parser.add_argument('--evaluations', type=bool, default=True, help='do evaluation at the end of the epoch w/ frequency')
@@ -56,7 +56,7 @@ def get_args():
     parser.add_argument('--latent-dim', type=int, default=10, help='Dimension of latent for goal encoder')
 
     # Curriculum learning arguments
-    parser.add_argument('--curriculum-learning', type=bool, default=True, help='Use LP-based curriculum learning')
+    parser.add_argument('--curriculum-learning', type=bool, default=False, help='Use LP-based curriculum learning')
     parser.add_argument('--curriculum-eps', type=float, default=0.3, help='Prob of sampling random goal in curriculum')
     parser.add_argument('--multihead-buffer', type=bool, default=True, help='use a multihead replay buffer in curriculum')
     parser.add_argument('--queue-length', type=int, default=1800, help='The window size when computing competence')
