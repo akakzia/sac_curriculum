5cda61b7e1559ea4696a16a979d7b0523f5cfb55
akakzia <ahmed.akakzia@gmail.com>
2020-11-17 21:30:35 +0100

    update demo for test opportunistic

--- demo.py
+++ demo.py
@@ -11,6 +11,7 @@ from types import SimpleNamespace
 from goal_sampler import GoalSampler
 import  random
 from mpi4py import MPI
+import pickle as pkl
 
 # process the inputs
 def normalize_goal(g, g_mean, g_std, args):
@@ -44,26 +45,30 @@ def get_env_params(env):
     return params
 
 if __name__ == '__main__':
-    num_eval = 10
-    path = '/home/flowers/Desktop/Scratch/sac_curriculum/ignoramus/2020-05-01 17:37:33.000157_curriculum_deepsets/'
-    model_path = path + 'model_0.pt'
+    num_eval = 100
+    path = '/home/ahakakzia/DECSTR/models/'
+    model_path = path + 'model_320.pt'
 
-    with open(path + 'config.json', 'r') as f:
-        params = json.load(f)
-    args = SimpleNamespace(**params)
-
+    # with open(path + 'config.json', 'r') as f:
+    #     params = json.load(f)
+    # args = SimpleNamespace(**params)
+    args = get_args()
 
     # Make the environment
     env = gym.make(args.env_name)
 
     # set random seeds for reproduce
-    args.seed = np.random.randint(1e6)
-    env.seed(args.seed + MPI.COMM_WORLD.Get_rank())
-    random.seed(args.seed + MPI.COMM_WORLD.Get_rank())
-    np.random.seed(args.seed + MPI.COMM_WORLD.Get_rank())
-    torch.manual_seed(args.seed + MPI.COMM_WORLD.Get_rank())
+    # args.seed = np.random.randint(1e6)
+    # rs = np.random.RandomState(seed=42)
+    with open('random_state.pkl', 'rb') as f:
+        rs = pkl.load(f)
+    seed = rs.randint(1e6)
+    env.seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
     if args.cuda:
-        torch.cuda.manual_seed(args.seed + MPI.COMM_WORLD.Get_rank())
+        torch.cuda.manual_seed(seed)
 
     args.env_params = get_env_params(env)
 
@@ -78,17 +83,26 @@ if __name__ == '__main__':
 
     # def rollout worker
     rollout_worker = RolloutWorker(env, policy, goal_sampler,  args)
-
+    # buckets = np.random.choice(np.arange(5), size=2)
+    # eval_goals = np.array([goal_sampler.oracle_id_to_g[np.random.choice(goal_sampler.buckets[b])] for b in buckets])
     eval_goals = goal_sampler.valid_goals
     inits = [None] * len(eval_goals)
     all_results = []
+    all_episodes = []
     for i in range(num_eval):
         episodes = rollout_worker.generate_rollout(eval_goals, self_eval=True, true_eval=True, animated=False)
+        print(i)
         if args.algo == 'continuous':
             results = np.array([e['rewards'][-1] == 3. for e in episodes]).astype(np.int)
         else:
             results = np.array([str(e['g_binary'][0]) == str(e['ag_binary'][-1]) for e in episodes]).astype(np.int)
         all_results.append(results)
+        all_episodes.append(episodes)
 
     results = np.array(all_results)
+    all_episodes = np.array(all_episodes)
+    # for e, b in zip(episodes, buckets):
+    #     e['bucket'] = b
     print('Av Success Rate: {}'.format(results.mean()))
+    with open('../rebuttal_continuous_2.pkl', 'wb') as f:
+        pkl.dump(all_episodes, f)
