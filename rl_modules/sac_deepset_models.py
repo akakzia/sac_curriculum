8afed88d4528da259459ae2eb00e26b88f425516
akakzia <ahmed.akakzia@gmail.com>
2020-11-18 15:01:15 +0100

    consider only two objects module and add distances

--- rl_modules/sac_deepset_models.py
+++ rl_modules/sac_deepset_models.py
@@ -128,11 +128,11 @@ class DeepSetSAC:
         self.dim_act = env_params['action']
         self.num_blocks = 3
         self.combinations_trick = args.combinations_trick
-        if self.combinations_trick:
-            self.n_permutations = len([x for x in combinations(range(self.num_blocks), 2)])
-        else:
-            self.n_permutations = len([x for x in permutations(range(self.num_blocks), 2)])
-        # self.n_permutations = 1
+        # if self.combinations_trick:
+        #     self.n_permutations = len([x for x in combinations(range(self.num_blocks), 2)])
+        # else:
+        #     self.n_permutations = len([x for x in permutations(range(self.num_blocks), 2)])
+        self.n_permutations = 1
 
 
 
@@ -239,9 +239,10 @@ class DeepSetSAC:
         if self.continuous_trick:
             dim_phi_actor_input = 2*6 + self.dim_body + dim_input_objects
         else:
-            dim_phi_actor_input = dim_input_goals + self.dim_body + dim_input_objects
+            dim_phi_actor_input = dim_input_goals + self.dim_body + dim_input_objects + 3 # for distances
 
-        dim_phi_actor_output = 3 * (self.dim_body + (self.num_blocks + self.dim_object))
+        # dim_phi_actor_output = 3 * (self.dim_body + (self.num_blocks + self.dim_object))
+        dim_phi_actor_output = 3 * dim_phi_actor_input
 
         dim_rho_actor_input = dim_phi_actor_output
         dim_rho_actor_output = self.dim_act
@@ -249,9 +250,10 @@ class DeepSetSAC:
         if self.continuous_trick:
             dim_phi_critic_input = 6*2 + self.dim_body + dim_input_objects + self.dim_act
         else:
-            dim_phi_critic_input = dim_input_goals + self.dim_body + dim_input_objects + self.dim_act
+            dim_phi_critic_input = dim_input_goals + self.dim_body + dim_input_objects + self.dim_act + 3 # for distances
 
-        dim_phi_critic_output = 3 * (self.dim_body + (self.num_blocks + self.dim_object) + self.dim_act)
+        # dim_phi_critic_output = 3 * (self.dim_body + (self.num_blocks + self.dim_object) + self.dim_act)
+        dim_phi_critic_output = 3 * dim_phi_critic_input
 
         dim_rho_critic_input = dim_phi_critic_output
         dim_rho_critic_output = 1
@@ -282,12 +284,11 @@ class DeepSetSAC:
                 encodings = torch.tensor(encodings, dtype=torch.float32).unsqueeze(0)
                 # goal_embeddings = self.policy_sentence_encoder.forward(encodings)[0][:, -1, :]
                 if UNIQUE_ENCODER:
-                    with torch.no_grad():
-                        goal_embeddings = self.critic_sentence_encoder.forward(encodings)[0][:, -1, :]
+                    goal_embeddings = self.critic_sentence_encoder.forward(encodings)[0][:, -1, :]
                 else:
                     goal_embeddings = self.policy_sentence_encoder.forward(encodings)[0][:, -1, :]
 
-
+        obs_distances = self.observation.narrow(-1, start=55, length=3) # dx, dy, dz between green and red
         obs_body = self.observation.narrow(-1, start=0, length=self.dim_body)
         obs_objects = [torch.cat((torch.cat(obs_body.shape[0] * [self.one_hot_encodings[i]]).reshape(obs_body.shape[0], self.num_blocks),
                                   self.observation.narrow(-1, start=self.dim_object*i + self.dim_body, length=self.dim_object)),
@@ -376,7 +377,9 @@ class DeepSetSAC:
                 input_actor = torch.stack(all_inputs)
             else:
                 if not self.include_ag:
-                    input_actor = torch.stack([torch.cat([body_input_actor, x[0], x[1]], dim=1) for x in permutations(obj_input_actor, 2)])
+                    # input_actor = torch.stack([torch.cat([body_input_actor, x[0], x[1]], dim=1) for x in permutations(obj_input_actor, 2)])
+                    input_actor = torch.stack([torch.cat([body_input_actor, obj_input_actor[0], obj_input_actor[1],
+                                                          obs_distances], dim=1)])
                 else:
                     input_actor = torch.stack([torch.cat([ag, body_input_actor, x[0], x[1]], dim=1) for x in permutations(obj_input_actor, 2)])
 
@@ -404,11 +407,11 @@ class DeepSetSAC:
                 # encodings = np.array([self.one_hot_language[str(sg)] for sg in g])
                 encodings = torch.tensor(encodings, dtype=torch.float32)
                 if UNIQUE_ENCODER:
-                    with torch.no_grad():
-                        goal_embeddings = self.critic_sentence_encoder.forward(encodings)[0][:, -1, :]
+                    goal_embeddings = self.critic_sentence_encoder.forward(encodings)[0][:, -1, :]
                 else:
                     goal_embeddings = self.policy_sentence_encoder.forward(encodings)[0][:, -1, :]
 
+        obs_distances = self.observation.narrow(-1, start=55, length=3)
         obs_body = self.observation[:, :self.dim_body]
         obs_objects = [torch.cat((torch.cat(batch_size * [self.one_hot_encodings[i]]).reshape(obs_body.shape[0], self.num_blocks),
                                obs[:, self.dim_body + self.dim_object * i: self.dim_body + self.dim_object * (i + 1)]), dim=1)
@@ -485,9 +488,10 @@ class DeepSetSAC:
 
             # Parallelization by stacking input tensors
             if not self.include_ag:
-                input_actor = torch.stack([torch.cat([body_input, x[0], x[1]], dim=1) for x in permutations(obj_input, 2)])
+                # input_actor = torch.stack([torch.cat([body_input, x[0], x[1]], dim=1) for x in permutations(obj_input, 2)])
+                input_actor = torch.stack([torch.cat([body_input, obs_objects[0], obs_objects[1], obs_distances], dim=1)])
             else:
-                input_actor = torch.stack([torch.cat([ag, body_input, x[0], x[1]], dim=1) for x in permutations(obj_input, 2)])            #input_actor = torch.stack([torch.cat([ag, body_input, x[0], x[1]], dim=1) for x in combinations(obj_input, 2)])
+                input_actor = torch.stack([torch.cat([ag, body_input, x[0], x[1]], dim=1) for x in permutations(obj_input, 2)])
 
         output_phi_actor = self.single_phi_actor(input_actor).sum(dim=0)
         if not eval:
