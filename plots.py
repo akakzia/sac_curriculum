14cb1c15c98ffa096a9d91793983fcd318c4d8e5
akakzia <ahmed.akakzia@gmail.com>
2020-11-19 11:47:38 +0100

    update plots for per instruction success rate

--- plots.py
+++ plots.py
@@ -24,7 +24,7 @@ colors = [[0, 0.447, 0.7410], [0.85, 0.325, 0.098], [0.466, 0.674, 0.188], [0.92
 
 RESULTS_PATH = '/home/ahakakzia/DECSTR/rebuttal/'
 SAVE_PATH = '/home/ahakakzia/DECSTR/rebuttal/'
-TO_PLOT = ['baselines']
+TO_PLOT = ['ignoramus']
 
 LINE = 'mean'
 ERR = 'std'
@@ -71,26 +71,26 @@ def setup_figure(xlabel=None, ylabel=None, xlim=None, ylim=None):
     return artists, ax
 
 def setup_n_figs(n, xlabels=None, ylabels=None, xlims=None, ylims=None):
-    fig, axs = plt.subplots(n, 1, figsize=(10, 10), frameon=False)
-    axs = axs.ravel()
+    fig, ax = plt.subplots(n, 1, figsize=(10, 10), frameon=False)
+    # axs = axs.ravel()
     artists = ()
-    for i_ax, ax in enumerate(axs):
-        ax.spines['top'].set_linewidth(3)
-        ax.spines['right'].set_linewidth(3)
-        ax.spines['bottom'].set_linewidth(3)
-        ax.spines['left'].set_linewidth(3)
-        ax.tick_params(width=4, direction='in', length=10, labelsize='15', zorder=10)
-        if xlabels[i_ax]:
-            xlab = ax.set_xlabel(xlabels[i_ax], fontsize='15')
-            artists += (xlab,)
-        if ylabels[i_ax]:
-            ylab = ax.set_ylabel(ylabels[i_ax], fontsize='15')
-            artists += (ylab,)
-        if ylims[i_ax]:
-            ax.set_ylim(ylims[i_ax])
-        if xlims[i_ax]:
-            ax.set_xlim(xlims[i_ax])
-    return artists, axs
+    # for i_ax, ax in enumerate(axs):
+    ax.spines['top'].set_linewidth(3)
+    ax.spines['right'].set_linewidth(3)
+    ax.spines['bottom'].set_linewidth(3)
+    ax.spines['left'].set_linewidth(3)
+    ax.tick_params(width=4, direction='in', length=10, labelsize='15', zorder=10)
+    if xlabels[0]:
+        xlab = ax.set_xlabel(xlabels[0], fontsize='15')
+        artists += (xlab,)
+    if ylabels[0]:
+        ylab = ax.set_ylabel(ylabels[0], fontsize='15')
+        artists += (ylab,)
+    if ylims[0]:
+        ax.set_ylim(ylims[0])
+    if xlims[0]:
+        ax.set_xlim(xlims[0])
+    return artists, ax
 
 def save_fig(path, artists):
     plt.savefig(os.path.join(path), bbox_extra_artists=artists, bbox_inches='tight', dpi=DPI)
@@ -141,65 +141,69 @@ def plot_c_lp_p_sr(experiment_path, true_buckets=True):
             x_eps = np.arange(0, len(data_run) * NB_EPS_PER_EPOCH, NB_EPS_PER_EPOCH * FREQ) / 1000
             x_eps = np.arange(0, len(data_run), FREQ)
             x = np.arange(0, len(data_run), FREQ)
-            artists, axs = setup_n_figs(n=6,
-                                        xlabels=['Epochs'] * 6,
+            artists, axs = setup_n_figs(n=1,
+                                        xlabels=['Epochs'] * 1,
                                         # xlabels=['Episodes (x$10^3$)'] * 4,
-                                        ylabels=['SR', '#R_config', 'R_continuous', 'C', 'LP', 'P'],
-                                        xlims=[(0, x_eps[-1])] * 6,
-                                        ylims=[(0,1), (0, 2000), (0, 3), (0, 1), None, (0, 1)])
-            if true_buckets:
-                buckets = generate_goals()
-                all_goals = generate_all_goals_in_goal_space().astype(np.float32)
-                valid_goals = []
-                for k in buckets.keys():
-                    valid_goals += buckets[k]
-                valid_goals = np.array(valid_goals)
-                all_goals = np.array(all_goals)
-                num_goals = all_goals.shape[0]
-                all_goals_str = [str(g) for g in all_goals]
-                # initialize dict to convert from the oracle id to goals and vice versa.
-                # oracle id is position in the all_goal array
-                g_str_to_oracle_id = dict(zip(all_goals_str, range(num_goals)))
-                valid_goals_oracle_ids = np.array([g_str_to_oracle_id[str(vg)] for vg in valid_goals])
-
-                bucket_ids = dict()
-                for k in buckets.keys():
-                    bucket_ids[k] = np.array([g_str_to_oracle_id[str(np.array(g))] for g in buckets[k]])
-                    id_in_valid = [int(np.argwhere(valid_goals_oracle_ids == i).flatten()) for i in bucket_ids[k]]
-                    sr = np.mean([data_run['Eval_SR_{}'.format(i)] for i in id_in_valid], axis=0)
-                    av_reward = np.mean([data_run['Av_Rew_{}'.format(i)] for i in id_in_valid], axis=0)
-                    axs[0].plot(x_eps, sr[x], color=colors[k], marker=MARKERS[k], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
-                    axs[2].plot(x_eps, av_reward[x], color=colors[k], marker=MARKERS[k], markersize=MARKERSIZE // 3, linewidth=LINEWIDTH // 2)
-
-            else:
-                T = len(data_run['Eval_SR_1'])
-                SR = np.zeros([NB_BUCKETS, T])
-                for t in range(T):
-                    for i in range(NB_BUCKETS):
-                        ids = []
-                        for g_id in range(35):
-                            if data_run['{}_in_bucket'.format(g_id)][t] == i:
-                                ids.append(g_id)
-                        values = [data_run['Eval_SR_{}'.format(g_id)][t] for g_id in ids]
-                        SR[i, t] = np.mean(values)
-
-                for i in range(SR.shape[0]):
-                    axs[0].plot(x_eps, SR[i][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
-                axs[0].plot(x_eps, np.mean(SR, axis=0)[x], color='k', linestyle='--', marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
+                                        ylabels=['SR'],
+                                        xlims=[(0, x_eps[-1])] ,
+                                        ylims=[(0,1)])
+            # if true_buckets:
+            #     buckets = generate_goals()
+            #     all_goals = generate_all_goals_in_goal_space().astype(np.float32)
+            #     valid_goals = []
+            #     for k in buckets.keys():
+            #         valid_goals += buckets[k]
+            #     valid_goals = np.array(valid_goals)
+            #     all_goals = np.array(all_goals)
+            #     num_goals = all_goals.shape[0]
+            #     all_goals_str = [str(g) for g in all_goals]
+            #     # initialize dict to convert from the oracle id to goals and vice versa.
+            #     # oracle id is position in the all_goal array
+            #     g_str_to_oracle_id = dict(zip(all_goals_str, range(num_goals)))
+            #     valid_goals_oracle_ids = np.array([g_str_to_oracle_id[str(vg)] for vg in valid_goals])
+            #
+            #     bucket_ids = dict()
+            #     for k in buckets.keys():
+            #         bucket_ids[k] = np.array([g_str_to_oracle_id[str(np.array(g))] for g in buckets[k]])
+            #         id_in_valid = [int(np.argwhere(valid_goals_oracle_ids == i).flatten()) for i in bucket_ids[k]]
+            #         sr = np.mean([data_run['Eval_SR_{}'.format(i)] for i in id_in_valid], axis=0)
+            #         av_reward = np.mean([data_run['Av_Rew_{}'.format(i)] for i in id_in_valid], axis=0)
+            #         axs[0].plot(x_eps, sr[x], color=colors[k], marker=MARKERS[k], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
+            #         axs[2].plot(x_eps, av_reward[x], color=colors[k], marker=MARKERS[k], markersize=MARKERSIZE // 3, linewidth=LINEWIDTH // 2)
+            #
+            # else:
+            #     T = len(data_run['Eval_SR_1'])
+            #     SR = np.zeros([NB_BUCKETS, T])
+            #     for t in range(T):
+            #         for i in range(NB_BUCKETS):
+            #             ids = []
+            #             for g_id in range(35):
+            #                 if data_run['{}_in_bucket'.format(g_id)][t] == i:
+            #                     ids.append(g_id)
+            #             values = [data_run['Eval_SR_{}'.format(g_id)][t] for g_id in ids]
+            #             SR[i, t] = np.mean(values)
+            #
+            #     for i in range(SR.shape[0]):
+            #         axs[0].plot(x_eps, SR[i][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
+            #     axs[0].plot(x_eps, np.mean(SR, axis=0)[x], color='k', linestyle='--', marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
 
             counter = 0
-            for i in range(NB_BUCKETS):
-                axs[3].plot(x_eps, data_run['B_{}_C'.format(i)][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
-                axs[4].plot(x_eps, data_run['B_{}_LP'.format(i)][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
-                axs[5].plot(x_eps, data_run['B_{}_p'.format(i)][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
+            for i in range(4):
+                axs.plot(x_eps, data_run['Eval_SR_{}'.format(i)][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE // 3,
+                            linewidth=LINEWIDTH // 2)
 
-                try:
-                    p = np.array([data_run['#Rew_{}'.format(i)] for i in range(counter, counter + len(buckets[i]))])
-                    counter += len(buckets[i])
-                    axs[1].plot(x_eps, np.mean(p, axis=0)[x],  color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//3)
-                except:
-                    pass
-            leg = axs[0].legend(['B{}'.format(i) for i in range(1, 6)],
+            # for i in range(NB_BUCKETS):
+            #     axs[3].plot(x_eps, data_run['B_{}_C'.format(i)][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
+            #     axs[4].plot(x_eps, data_run['B_{}_LP'.format(i)][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
+            #     axs[5].plot(x_eps, data_run['B_{}_p'.format(i)][x], color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//2)
+
+                # try:
+                #     p = np.array([data_run['#Rew_{}'.format(i)] for i in range(counter, counter + len(buckets[i]))])
+                #     counter += len(buckets[i])
+                #     axs[1].plot(x_eps, np.mean(p, axis=0)[x],  color=colors[i], marker=MARKERS[i], markersize=MARKERSIZE//3, linewidth=LINEWIDTH//3)
+                # except:
+                #     pass
+            leg = axs.legend(['I{}'.format(i) for i in range(1, 6)],
                              loc='upper center',
                              bbox_to_anchor=(0.5, 1.25),
                              ncol=5,
