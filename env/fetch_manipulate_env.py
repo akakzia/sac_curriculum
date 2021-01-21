c2d1dfd2e3102b8c437d532733fe1a1f27eb4207
akakzia <ahmed.akakzia@gmail.com>
2021-01-21 13:20:03 +0100

    test put current predicates in masked bits

--- env/fetch_manipulate_env.py
+++ env/fetch_manipulate_env.py
@@ -251,19 +251,19 @@ class FetchManipulateEnv(robot_env.RobotEnv):
         achieved_goal = np.squeeze(achieved_goal)
 
         # Reflect achieved goal in goal according to mask
-        # if self.mask is not None:
-        #     desired_goal = achieved_goal * self.mask + self.target_goal * (1 - self.mask)
-        # else:
-        #     desired_goal = self.target_goal.copy()
-        #
-        # desired_goal = np.squeeze(desired_goal)
+        if self.mask is not None:
+            desired_goal = achieved_goal * self.mask + self.target_goal * (1 - self.mask)
+        else:
+            desired_goal = self.target_goal.copy()
+
+        desired_goal = np.squeeze(desired_goal)
 
         return {
             'observation': obs.copy(),
             'achieved_goal': achieved_goal.copy(),
-            'desired_goal': self.target_goal.copy(),
+            'desired_goal': desired_goal.copy(),
             'achieved_goal_binary': achieved_goal.copy(),
-            'desired_goal_binary': self.target_goal.copy()}
+            'desired_goal_binary': desired_goal.copy()}
 
     def _viewer_setup(self):
         body_id = self.sim.model.body_name2id('robot0:gripper_link')
