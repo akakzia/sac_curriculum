07d6fca5078e1701014db7c28ad6abdc3dbbd2d7
akakzia <ahmed.akakzia@gmail.com>
2021-04-04 18:25:10 +0200

    consider 3 and ignore 2 objects

--- train.py
+++ train.py
@@ -170,8 +170,10 @@ def launch(args):
                                                        biased_init=False,
                                                        language_goal=language_goal)
 
-            results = np.array([e['success'][-1].astype(np.float32) for e in episodes])
-            rewards = np.array([e['rewards'][-1] for e in episodes])
+            rewards = np.array([policy.her_module.compute_reward_masks(episodes[i]['ag'][-1], eval_goals[i], None) for i in range(len(eval_goals))])
+            results = np.array([(r == 3.).astype(np.float32) for r in rewards])
+            # results = np.array([e['success'][-1].astype(np.float32) for e in episodes])
+            # rewards = np.array([e['rewards'][-1] for e in episodes])
             all_results = MPI.COMM_WORLD.gather(results, root=0)
             all_rewards = MPI.COMM_WORLD.gather(rewards, root=0)
             time_dict['eval'] += time.time() - t_i
