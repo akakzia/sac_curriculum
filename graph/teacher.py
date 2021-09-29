3adfafda48912890d65a747dd493e507249c7878
ahmed <ahmed.akakzia@gmail.com>
2021-09-29 18:47:33 +0200

    implement different ablations

--- graph/teacher.py
+++ graph/teacher.py
@@ -11,6 +11,8 @@ class Teacher():
         self.oracle_graph = SemanticGraph.load_oracle(args.n_blocks)
         self.args = args
         self.agent_frontier = {} # store configuration through networkit node_id from agent_graph
+        self.agent_stepping_stones = {}
+        self.agent_frontier_not_stepping_stones = {}
         with open('data/classes_and_configs.pkl', 'rb') as f:
             self.config_to_class, _ = pkl.load(f)
 
@@ -36,28 +38,82 @@ class Teacher():
                 return True
         return False
 
+    def is_stepping_stone(self, config, agent_graph: SemanticGraph):
+        '''
+        Compute the set of nodes that are stepping stones to the unknown
+        '''
+        if not self.oracle_graph.hasNode(config):
+            return False
+
+        neighbours = self.oracle_graph.iterNeighbors(config)
+        for neighbour in neighbours:
+            # if not agent_graph.hasNode(neighbour):
+            if not agent_graph.hasEdge(config, neighbour):
+                return True
+        return False
+
+    def is_frontier_not_stepping_stone(self, config, agent_graph: SemanticGraph):
+        '''
+        Compute the set of nodes that are not stepping stones but are at the frontier
+        '''
+        if not self.oracle_graph.hasNode(config):
+            return False
+
+        if agent_graph.getNodeId(config) in agent_graph.frontier:
+            return True
+        return False
+
+
     def computeFrontier(self,agent_graph:SemanticGraph):
         self.agent_frontier = set()
+        self.agent_stepping_stones = set()
+        self.agent_frontier_not_stepping_stones = set()
         for node in agent_graph.configs:
-            if self.is_in_frontier(node,agent_graph):
-                self.agent_frontier.add( agent_graph.getNodeId(node))
-        
-    def sample_in_frontier(self,current_node,agent_graph,k):
+            # if self.is_in_frontier(node,agent_graph):
+            #     self.agent_frontier.add( agent_graph.getNodeId(node))
+            if self.is_stepping_stone(node,agent_graph):
+                self.agent_frontier.add(agent_graph.getNodeId(node))
+                self.agent_stepping_stones.add(agent_graph.getNodeId(node))
+                continue
+            elif self.is_frontier_not_stepping_stone(node, agent_graph):
+                self.agent_frontier.add(agent_graph.getNodeId(node))
+                self.agent_frontier_not_stepping_stones.add(agent_graph.getNodeId(node))
+
+    def sample_in_frontier(self,current_node,agent_graph,k, ablation=5):
         reachables = agent_graph.get_reachables_node_ids(current_node)
         reachable_frontier = [agent_graph.getConfig(node_id) 
                               for node_id in reachables 
-                              if node_id in self.agent_frontier] 
-        if reachable_frontier:
-            goals = random.choices(reachable_frontier, k=k)  # sample with replacement
-            for g in goals:
-                try:
-                    c = self.config_to_class[str(np.array(g).reshape(1, -1))]
-                    self.stats[c+1] += 1
-                except KeyError:
-                    pass
-            return goals
-        else: 
-            return []
+                              if node_id in self.agent_frontier]
+        reachable_stepping_stones = [agent_graph.getConfig(node_id)
+                              for node_id in reachables
+                              if node_id in self.agent_stepping_stones]
+        reachable_frontier_not_stepping_stones = [agent_graph.getConfig(node_id)
+                                                  for node_id in reachables
+                                                  if node_id in self.agent_frontier_not_stepping_stones]
+        if ablation != 2:
+            if reachable_frontier:
+                goals = random.choices(reachable_frontier, k=k)  # sample with replacement
+                for g in goals:
+                    try:
+                        c = self.config_to_class[str(np.array(g).reshape(1, -1))]
+                        self.stats[c+1] += 1
+                    except KeyError:
+                        pass
+                return goals
+            else:
+                return []
+        else:
+            if reachable_stepping_stones:
+                goals = random.choices(reachable_stepping_stones, k=k)  # sample with replacement
+                for g in goals:
+                    try:
+                        c = self.config_to_class[str(np.array(g).reshape(1, -1))]
+                        self.stats[c + 1] += 1
+                    except KeyError:
+                        pass
+                return goals
+            else:
+                return []
 
     def sample_from_frontier(self,node,agent_graph,k):
         to_explore = []
